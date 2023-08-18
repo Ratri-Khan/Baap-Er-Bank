d21530e4d9f9ec178aca692cbe4466badb892e3d
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-08-18 21:00:59 +0600

    finish

--- bank.js
+++ bank.js
@@ -1,27 +1,43 @@
 const depositSubmitBtn = document.getElementById('depositSubmit-btn').addEventListener('click',function(){
     const depositInput = document.getElementById('deposit-input');
     const newdeposit = depositInput.value;
+    depositInput.value ='';
     const newDepositAmount = parseFloat(newdeposit);
+
     const previousDeposit = document.getElementById('deposit');
     const previousDepositText = previousDeposit.innerText;
     const previousDepositAmount = parseFloat(previousDepositText);
-    // depositContainer.innerText = deposit;
-    const newDepositTotal = newDepositAmount + previousDepositAmount;
-    previousDeposit.innerText = newDepositTotal; 
+
+    const DepositTotal = newDepositAmount + previousDepositAmount;
+    previousDeposit.innerText = DepositTotal; 
+
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceText =  totalBalance.innerText;
+    const totalBalanceAmount = parseFloat(totalBalanceText);
+
+    const balace = totalBalanceAmount + newDepositAmount;
+    totalBalance.innerText = balace;
  })
 
+document.getElementById('withdrawSubmitBtn').addEventListener('click',function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawInputText = withdrawInput.value;
+    const NewwithdrawAmount = parseFloat(withdrawInputText);
+    withdrawInput.value = '';
+
+    const PreWithdraw = document.getElementById('withdraw');
+    const PreWithdrawText = PreWithdraw.innerText;
+    const preWithdrawAmount = parseFloat(PreWithdrawText);
+
+    const totalWithdraw = preWithdrawAmount + NewwithdrawAmount;
+    PreWithdraw.innerText = totalWithdraw;
+    
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceText =  totalBalance.innerText;
+    const totalBalanceAmount = parseFloat(totalBalanceText);
+
+    const balace = totalBalanceAmount - NewwithdrawAmount;
+    totalBalance.innerText = balace;
 
 
-
-
-
-
-
-
-
-//  const withdrawSubmitBtn = document.getElementById('withdrawSubmitBtn').addEventListener('click',function(){
-//     const withdrawInput = document.getElementById('withdraw-input');
-//     const withdraw = withdrawInput.value;
-//     const withdrawContainer = document.getElementById('withdraw');
-//     withdrawContainer.innerText = withdraw;
-//  })
+})
